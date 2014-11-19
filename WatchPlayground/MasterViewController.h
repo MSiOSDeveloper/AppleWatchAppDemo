302cb3065fb03e6b690388b83dca515f930c3ebe
Liangjun Jiang <lj@buyfrom.farm>
2014-11-19 10:28:51 -0600

    Initial Commit

---
+++ WatchPlayground/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  WatchPlayground
+//
+//  Created by Liangjun Jiang on 11/19/14.
+//  Copyright (c) 2014 Liangjun Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ WatchPlayground/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WatchPlayground
+//
+//  Created by Liangjun Jiang on 11/19/14.
+//  Copyright (c) 2014 Liangjun Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
